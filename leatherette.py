6fcdd3c83936bd203668b7ddb8932884de6d44d4
wryandginger <77764889+wryandginger@users.noreply.github.com>
2026-04-11 16:54:11 -0700

    Update leatherette.py
    channel icon fix (potentially?)

--- leatherette.py
+++ leatherette.py
@@ -195,7 +195,7 @@ def main():
                               text='%s %s' % (c_in['channelNo'], c_in['callSign']))
                 sub_el(c_out, 'display-name', text=c_in['channelNo'])
                 sub_el(c_out, 'display-name', text=c_in['callSign'])
-                channel_thumb = str(c_in['thumbnail']).replace("//zap2it", "https://emby").split("?")[0]
+                channel_thumb = str(c_in['thumbnail']).replace("//", "https://").split("?")[0]
                 sub_el(c_out, 'icon', src=channel_thumb)
 
         for c in d['channels']:
